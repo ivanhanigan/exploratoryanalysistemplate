e2adf987f9ee19789421e7f9173b2f2413abb4ac
ivanhanigan <ivanhanigan@gmail.com>
2015-04-15 21:44:32 +1000

    add work toward other modules

--- R/exploratory_analysis_template.r
+++ R/exploratory_analysis_template.r
@@ -17,45 +17,12 @@ exploratory_analysis_template <- function(
   ,
     show_geo = T  
     ){
-  
-txt <- taxonomic_template(target_col = "species")
+txt1 <- cleaning_template(datevar = "date")  
+txt2 <- taxonomic_template(target_col = "species")
+txt <- paste(txt1, txt2)
 if(show_header){
-hd <- sprintf('
-%s
----
-
-```{r echo = T, results = "asis", eval = T}
-projdir <- "%s"
-outdir <- file.path(projdir, "%s")
-# if(!file.exists(outdir)) dir.create(outdir)
-outfile <- "%s"
-file.path(outdir, outfile) 
-setwd(outdir)
-```
-```{r, echo = TRUE, eval = FALSE}
-library(gdata)
-library(disentangle)
-library(EML)
-library(sqldf)
-library(taxize)
-indir <- "%s"
-
-dir(indir)
-infile <- "%s"
-
-#### load ####
-print(file.path(indir,infile))
-dat <- read.csv(file.path(indir,infile), stringsAsFactors = F)
-  
-#### check ####
-str(dat)
-
-head(dat) 
-tail(dat)
-```
-', titl, projdir, packagedir, outfile, indir, infile
-)
-txt <- paste(hd, txt) 
+  hd <- header_template(titl=titl, projdir=projdir, packagedir=packagedir, outfile=outdir, indir=indir, infile=infile)
+  txt <- paste(hd, txt) 
 }
 
 # cat(txt)
--- R/taxonomic_review_template.r
+++ R/taxonomic_review_template.r
@@ -7,22 +7,24 @@ taxonomic_template <- function(target_col = "species"){
 txt <- paste('
 ```{r echo = T, results = "asis", eval = F}
 #### Taxonomic review ####
-tx <- as.data.frame(table(dat$',target_col,'))
-names(tx) <- c("',target_col,'", "Frequency")
-write.csv(tx, file.path(outdir, gsub(".csv","_taxonomic_coverage.csv", outfile)), row.names = F)  
-# Test new version?
+## to just simply summarise the species names
+# tx <- as.data.frame(table(dat$',target_col,'))
+# names(tx) <- c("',target_col,'", "Frequency")
+# write.csv(tx, file.path(outdir, gsub(".csv","_taxonomic_coverage.csv", outfile)), row.names = F)  
+## to use taxize to check the list do the following 
 tx <- as.data.frame(table(dat$',target_col,'))
 names(tx) <- c("',target_col,'", "Frequency")
 splist <- tx$',target_col,'
 sources <- gnr_datasources()
-sources
-
+#sources
+## the following lists have been selected as likely to be reasonable in australia
 eol <- sources$id[sources$title == "EOL"]
 gbif_backbone <- sources$id[sources$title == "GBIF Backbone Taxonomy"]
 ipni <- sources$id[sources$title == "The International Plant Names Index"]
 zk <- sources$id[sources$title == "ZooKeys"]
 zb <- sources$id[sources$title == "ZooBank"]
-c(eol, gbif_backbone, ipni, zk, zb)
+#c(eol, gbif_backbone, ipni, zk, zb)
+## this next big uses the internet to check lists
 out <- gnr_resolve(splist, data_source_ids=c(eol, gbif_backbone, ipni, zk, zb), stripauthority=TRUE)
 
 out2 <- unique(out$results)
@@ -32,15 +34,17 @@ out3 <- sqldf(\'select submitted_name, matched_name2 as match_via_database, max(
 out3[which(out3$submitted_name == out3$match_via_database),"max_database_score"] <- ""
 out3[which(out3$submitted_name == out3$match_via_database),"match_via_database"] <- ""
 out3
-write.csv(out3, file.path(outdir, gsub(".csv","_taxonomic_coverage.csv", outfile)), row.names = F)  
+## write out the result for clerical review
+tx_file <- gsub(".csv","_taxonomic_coverage.csv", outfile)
+write.csv(out3, file.path(outdir, tx_file), row.names = F)  
 #### TODO: 
 # you should go to this CSV file and edit the final columns, 
-# take notes on decisions and create the updates list.  Save as new file with 2.csv at end.
+# take notes on decisions and create the updates list.  Save.
 ####
 
 # Post review merge fixed names and remove old names
+# check the tx file is there, appropriately named
 dir(outdir)
-tx_file <- "xxx_taxonomic_coverage2.csv"
 tx <- read.csv(file.path(outdir, tx_file), stringsAsFactor = F)
 nrow(tx)
 head(tx)
@@ -48,18 +52,31 @@ tx[tx[,grep("change_note", names(tx))]!="",]
 str(dat)
 
 # check that linking variable is identical
-idx <- names(table(dat$',target_col,'))
+# there are a variety of reasons why these may have gotten out of sync during the clerical review
+idx <- as.data.frame(names(table(dat$",target_col,")))
+names(idx) <- "v1"
 head(idx)
-idy <- tx$submitted_name
+idy <- as.data.frame(tx$submitted_name)
+names(idy) <- "v1"
 head(idy)
-idx[-which(idx in idy)]
-idy[-which(idy in idx)]
+# which target do not appear in source
+sqldf("select *
+from idx
+left join idy
+on idx.v1 = idy.v1
+where idy.v1 is null")
+# which source do not appear in target
+sqldf("select *
+from idy
+left join idx
+on idy.v1 = idx.v1
+where idx.v1 is null")
 
 # if all good then merge
 dat <- merge(dat, tx, by.x = "',target_col,'", by.y = "submitted_name", all.x = T)
 str(dat)
 
-# reorder cols
+# reorder cols (col names need to be changed)
 paste(names(dat), collapse = "\',\'", sep = "")
 namelist <- c("visitcode","surveyyear", "',target_col,'")
 
