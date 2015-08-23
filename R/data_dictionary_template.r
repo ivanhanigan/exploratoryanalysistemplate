0f3be5ca810e7e1771798decaebe8c5afa7172c3
ivanhanigan <ivanhanigan@gmail.com>
2015-08-23 12:55:54 +1000

    add variable labels as a function

--- R/data_dictionary_template.r
+++ R/data_dictionary_template.r
@@ -9,29 +9,8 @@ txt <- paste('
 ## do a data dictionary and add the variable names and types
 dd <- data_dictionary(show_levels = 20, dat)
 write.csv(dd, file.path(outdir, gsub(".csv","_data_dictionary.csv", outfile)), row.names = F)
-
-#### now get variable names ####
-dd$ordering <- 1:nrow(dd)
-col_defs <- sqldf("
-select Variable, Type
-from dd
-group by Variable, Type
-order by ordering
-")
-col_defs <- col_defs[col_defs$Variable != "",]
-col_defs
-
-## the original names are treated as the labels.  To be expanded on during DDF
-variable_labels <- read.csv(file.path(indir,infile), nrows = 1, header = F, stringsAsFactors = F)
-variable_labels
-vl <- as.data.frame(cbind(col_defs,t(variable_labels[1,])))
-names(vl) <- c("ltern_name", "simple_type", "original_name")
-vl$description <- ""
-vl$nominal_ordinal_interval_ratio_date_time <- ""
-vl$unit_of_measurement <- ""
-vl$value_labels <- ""
-vl$issue_description_and_suggested_change <- ""
-vl$depositor_response <- ""
+vl <- variable_names_and_labels(
+  infile = file.path(indir, infile), datadict = dd, insert_labels = T)
 vl
 write.csv(vl, file.path(outdir, gsub(".csv","_variable_names_and_labels.csv", outfile)), row.names = F)
 
